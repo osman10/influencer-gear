921ae70e3f80a3ca2f3c61746a9c635e49978d91
Osman Goni <osman.mirpur55@gmail.com>
2026-01-18 20:12:28 +0600

    influencer gear

---
+++ script.js
@@ -0,0 +1,9 @@
+// search bar hide and show
+  function toggleDiv() {
+    var div = document.getElementById("navmenu");
+    if (div.style.display === "none") {
+      div.style.display = "block";
+    } else {
+      div.style.display = "none";
+    }
+  }
